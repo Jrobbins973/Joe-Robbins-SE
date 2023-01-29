c08166da8ebb8259acf2617ccfaa89092e8c87e6
Joe Robbins <joe.robbins.cpt@gmail.com>
2023-01-28 19:57:25 -0500

    initial things

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import AboutMe from './components/AboutMe';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='header'>
+        <img src = "https://i.imgur.com/SzaZ9l2.png"/>
+      </div>
+
+    <div className='grid-container'>
+      <AboutMe />
+      <AboutMe />
+      <AboutMe />
+      <AboutMe />
+      <AboutMe />
+      <AboutMe />
+      <AboutMe />
+      <AboutMe />
+      </div>
+
+{/* 
+    <div class="area" >
+            <ul class="circles">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+            </ul>
+    </div > */}
     </div>
   );
 }
---
+++ src/components/AboutMe.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function AboutMe() {
+return (
+    <div>
+
+    <div className='about-me-box'>
+        <h1>About Me</h1>
+        <p>I am a former exercise physiologist and researcher who is now making a career transition into software engineering. I have a strong background in the field of health and wellness and I am excited to apply my analytical and problem-solving skills to the development of new technology and software.  </p>
+        <br></br>
+        <p>I believe that my unique background and experience will bring a valuable perspective to the field of software engineering. I am excited to take on this new challenge and see where it takes me.</p>
+    </div>
+    </div>
+)
+}
+
+export default AboutMe
