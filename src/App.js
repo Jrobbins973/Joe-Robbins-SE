ec8d3bb8229615aba2e581f3f0b4491e2cc19b77
Joe Robbins <joe.robbins.cpt@gmail.com>
2023-01-29 18:21:18 -0500

    made it so divs float in when page is scrolled

--- src/App.js
+++ src/App.js
@@ -3,20 +3,39 @@ import './App.css';
 import AboutMe from './components/AboutMe';
 import MyBackground from './components/MyBackground';
 import { FaLinkedinIn } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaMedium } from "react-icons/fa";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
-
-
+AOS.init()
 
 function App() {
   return (
     <div className="App">
       <div className='header'>
         <img src = "https://i.imgur.com/SzaZ9l2.png"/>
+          <div className='social-icons'>
+          <a href='https://www.linkedin.com/in/joe-robbins-swe/' target="_blank"><FaLinkedinIn size={22}/></a>
+          <a href='https://github.com/Jrobbins973' target="_blank"><FaGithub size={22}/></a>
+          <a href='https://medium.com/@joe.robbins.cpt' target="_blank"><  FaMedium size={22}/></a>
+          </div>
       </div>
 
     <div className='grid-container'>
       <AboutMe />
-      <MyBackground />
+      <AboutMe />
+
+        <div data-aos="fade-up" 
+            data-aos-delay="50"
+            data-aos-duration="1000"
+            data-aos-easing="ease-in-out">
+        <MyBackground />
+        </div>
+
+        
+      
+      
       </div>
 
 
